7a5348f6e0005c35a8b669d7eae7dbe1cc8083ac
Josh N <jnezia1@hotmail.com>
2022-04-13 12:13:27 -0500

    Setup templates for team members
    Also, I installed Jinja2.

--- venv/home_page.py
+++ venv/home_page.py
@@ -1,22 +1,29 @@
+from email import message
 from flask import Flask, render_template
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = Flask(__name__)
-
+env = Environment(
+    loader=PackageLoader("app"),
+    autoescape=select_autoescape()
+)
 @app.route('/')
-def test_output():
-    return 'Hello Katherine, Connie, and Lap '
+def root():
+    message = "This is our homepage"
+    return render_template("index.html", msg=message)
 
-@app.route('/login')
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/connie-page')
 def login():
-    return 'login'
+    data = ""
+    return render_template("conn.html")
 
 @app.route('/user/<username>')
 def profile(username):
     return f'{username}\'s profile'
-
-@app.route('/about')
-def about():
-    return 'about'
 
 @app.route('/test')
 # @app.route('/test/<name>')
